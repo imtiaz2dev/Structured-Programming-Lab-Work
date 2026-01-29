84cf36ec6c678418221bbc177bc4289a5ab8f6e6
Ahmed Imtiaz <azimti013@gmail.com>
2026-01-29 10:49:28 +0600

    Add My Details.c with personal information

---
+++ My Details.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+
+    printf("Name: Ahmed Imtiaz\nId: 252002053\nDept Name: Computer Science & Engineering\nUniversity name: Green University of Bangladesh");
+    return 0;
+}

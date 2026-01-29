0493c6176c609a5c7b7a21be87a34649420abccf
Ahmed Imtiaz <azimti013@gmail.com>
2026-01-29 12:01:20 +0600

    Add program to calculate average of two numbers

---
+++ Average 2number.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  float x,y,avg;
+  printf("Enter the first number: ");
+  scanf("%f",&x);
+  printf("Enter the second number: ");
+  scanf("%f",&y);
+avg=(x+y)/2;
+  printf("Average is: %.2f",avg);
+
+return 0;
+}

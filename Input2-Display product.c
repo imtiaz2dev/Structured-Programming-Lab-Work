925bd33dbce1d4035940a1e38c9b1df02b91e3cc
Ahmed Imtiaz <azimti013@gmail.com>
2026-01-29 11:26:08 +0600

    Add program to display product of two numbers

---
+++ Input2-Display product.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  int x,y,product;
+  printf("Enter the first number: ");
+  scanf("%d",&x);
+  printf("Enter the second number: ");
+  scanf("%d",&y);
+product=x*y;
+  printf("%d*%d= %d",x,y,product);
+
+return 0;
+}

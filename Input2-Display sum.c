ce9bf40faa2110a370266fd919a7aca3816246a0
Ahmed Imtiaz <azimti013@gmail.com>
2026-01-29 11:22:08 +0600

    Add program to display the sum of two numbers

---
+++ Input2-Display sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+  int x,y,sum;
+  printf("Enter the first number: ");
+  scanf("%d",&x);
+  printf("Enter the Second number: ");
+  scanf("%d",&y);
+sum=x+y;
+  printf("%d+%d= %d",x,y,sum);
+  
+return 0;
+}

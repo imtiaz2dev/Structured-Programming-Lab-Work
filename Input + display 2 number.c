2200a9147c3ab24190ba5c9faf34532cdfae9ccf
Ahmed Imtiaz <azimti013@gmail.com>
2026-01-29 11:17:03 +0600

    Add program to input and display two numbers

---
+++ Input + display 2 number.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+  int x,y;
+printf("Enter The First Number: ");
+scanf("%d",&x);
+printf("Enter The Second Number: ");
+scanf("%d",&y);
+
+printf("First Number is: %d\n",x);
+printf("Second Number is: %d",y);
+
+return 0;
+}

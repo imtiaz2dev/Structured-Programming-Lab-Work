24889d81195cd5fc90fd8be43fed2b6684563677
Ahmed Imtiaz <azimti013@gmail.com>
2026-02-01 13:01:19 +0600

    Add Pass or Fail program in C

---
+++ Pass or Fail.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    int result;
+    printf("Enter result: ");
+    scanf("%d",&result);
+if(result>=40){
+    printf("Pass");
+}else{
+    printf("Fail");
+}
+return 0;
+}

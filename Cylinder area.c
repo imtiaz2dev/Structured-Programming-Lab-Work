42ddd12cdf0898118ecc57657921ae2e813e0689
Ahmed Imtiaz <azimti013@gmail.com>
2026-02-18 09:22:31 +0600

    Add program to calculate cylinder volume

---
+++ Cylinder area.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+    float pi=3.1416;
+    float r,h,x;
+    printf("Enter Radius: ");
+scanf("%f",&r);
+    printf("Enter Hight: ");
+scanf("%f",&h);
+
+x=pi*r*r*h;
+    printf("Volume: %f",x);
+
+    return 0;
+}

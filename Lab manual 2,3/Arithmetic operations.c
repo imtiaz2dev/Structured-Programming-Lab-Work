a385f38a0a029b02b018c10eb602f6beab6c073c
Ahmed Imtiaz <azimti013@gmail.com>
2026-02-20 12:19:04 +0600

    Add multiplication operation in Arithmetic operations

---
+++ Lab manual 2,3/Arithmetic operations.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    
+int x,y,z;
+    printf("Enter 1st number: ");
+scanf("%d",&x);
+    printf("Enter 2nd number: ");
+    scanf("%d",&y);
+z=x*y;
+    printf("Product is: %d",z);
+
+return 0;
+}

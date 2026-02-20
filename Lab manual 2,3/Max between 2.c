6f7380ca2601cf30e042f7dd69db5604600908ea
Ahmed Imtiaz <azimti013@gmail.com>
2026-02-20 13:03:14 +0600

    Add program to find the maximum between two numbers

---
+++ Lab manual 2,3/Max between 2.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main(){
+    
+    int a,b;
+
+printf("Enter 1st number: ");
+    scanf("%d",&a);
+printf("Enter 2nd number: ");
+    scanf("%d",&b);
+
+if(a>b){
+    printf("1st number is big");
+}else{
+    printf("2nd number is big");
+}
+
+return 0;
+}

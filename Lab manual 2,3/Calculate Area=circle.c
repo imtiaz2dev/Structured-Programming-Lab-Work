57ab7f77eb51c453f6e2653f8aa80043c26d0e7e
Ahmed Imtiaz <azimti013@gmail.com>
2026-02-20 12:24:17 +0600

    Add circle area calculation program

---
+++ Lab manual 2,3/Calculate Area=circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    
+    float x,y,pi=3.1416;
+    printf("Enter Radius: ");
+scanf("%f",&x);
+    
+y=x*x*pi;
+    printf("Area of cirlce is: %.4f",y);
+
+return 0;
+}

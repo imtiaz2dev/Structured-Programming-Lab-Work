d738a0ac76f6dca9dc0094afa13eb4d0c59483f1
Ahmed Imtiaz <azimti013@gmail.com>
2026-02-20 12:53:38 +0600

    Add program to calculate average of five subjects

---
+++ Lab manual 2,3/5mark avg.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    
+    int a,b,c,d,e,f;
+    printf("Bangla= ");
+scanf("%d",&a);
+    printf("English= ");
+scanf("%d",&b);
+    printf("Math= ");
+scanf("%d",&c);
+    printf("Science= ");
+scanf("%d",&d);
+    printf("Islam= ");
+scanf("%d",&e);
+    
+f=(a+b+c+d+e)/5;
+    printf("Avg of total result: %d",f);
+
+return 0;
+}

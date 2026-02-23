aefd3bf26988f0cd50de966c068d1d520c33b364
Ahmed Imtiaz <azimti013@gmail.com>
2026-02-23 06:58:10 +0600

    Update C Converter.c

--- Lab manual 2,3/C Converter.c
+++ Lab manual 2,3/C Converter.c
@@ -2,11 +2,11 @@
 
 int main(){
     float C,F;
-printf("Enter Fahrenheit: ");
-scanf("%f",&F);
-C=(F-32)*5/9;
+printf("Enter Celsius: ");
+scanf("%f",&C);
+F=(C*1.8)+32;
 
-printf("Celsius: %.2f",C);
+printf("Fahrenheit: %.2f",F);
 
 return 0;
 }

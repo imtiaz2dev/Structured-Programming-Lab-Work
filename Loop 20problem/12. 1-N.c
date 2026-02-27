bbf4ee56c778d344e0b1e12f6baba442f51203da
Ahmed Imtiaz <azimti013@gmail.com>
2026-02-27 08:44:08 +0600

    Add program to print numbers from 1 to N

---
+++ Loop 20problem/12. 1-N.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int x=1,y;
+    printf("Enter Digit: ");
+    scanf("%d",&y);
+    while(x<=y){
+    printf("%d\n",x);
+    x++;
+    }
+   
+    return 0;
+}

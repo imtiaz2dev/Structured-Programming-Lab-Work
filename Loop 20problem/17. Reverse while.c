26e3be69c6682d2a7917df1ece3c92a1e75028e8
Ahmed Imtiaz <azimti013@gmail.com>
2026-02-27 10:35:35 +0600

    Implement integer reversal using a while loop
    Add a program to reverse the digits of an integer using a while loop.

--- Loop 20problem/17. Reverse while.c
+++ Loop 20problem/17. Reverse while.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main() {
+    int x,y,z;
+    scanf("%d", &x);
+    y=x;
+    while(y>0){
+        z=y%10;
+        printf("%d", z);
+        y=y/10;
+       }
+    return 0;
+}
